79358f4f440628f01160fff850350ad65bb95737
Andreas Suurkuusk <andreas@scitech.se>
2019-10-16 12:06:54 +0200

    Add TaskEx class with Run methods taking a TaskScheduler argument.

---
+++ src/SciTech.Core/Threading/TaskEx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SciTech.Core.Threading
+{
+    public static class TaskEx
+    {
+        public static Task Run(Action action, TaskScheduler scheduler, CancellationToken cancellationToken)
+            => Task.Factory.StartNew(
+                action,
+                cancellationToken,
+                TaskCreationOptions.DenyChildAttach,
+                scheduler);
+
+        public static Task Run(Action action, TaskScheduler scheduler)
+            => Task.Factory.StartNew(
+                action,
+                CancellationToken.None,
+                TaskCreationOptions.DenyChildAttach,
+                scheduler);
+
+    }
+}

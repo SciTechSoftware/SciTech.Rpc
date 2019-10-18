68fa9dc60e55cb47e21f1f31382e7b1a4ab1eec6
Andreas Suurkuusk <andreas@scitech.se>
2019-10-18 10:58:26 +0200

    Update Ticketer example to use modified IRpcCallContext.

--- examples/Clients/Ticketer/Program.cs
+++ examples/Clients/Ticketer/Program.cs
@@ -24,7 +24,7 @@ namespace TicketerClient
             {
                 if (token != null)
                 {
-                    metadata.AddValue("Authorization", $"Bearer {token}");
+                    metadata.AddHeader("Authorization", $"Bearer {token}");
                 }
 
                 return Task.CompletedTask;
